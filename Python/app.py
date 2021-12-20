ea55df8c5afcc8bf084f6272dd21ebdffa195950
cyanedel <cleewlkr@gmail.com>
2021-12-21 03:29:42 +0700

    Add Python WS

---
+++ Python/app.py
@@ -0,0 +1,14 @@
+import json
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+with open('countryList.json', errors="ignore") as f:
+  countryList = json.load(f)
+
+@app.get("/getcountrylist")
+def get_countries():
+    return jsonify(countryList)
+
+if __name__ == '__main__':
+  app.run()
