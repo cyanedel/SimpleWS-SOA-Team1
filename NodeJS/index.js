bfbf87bbfe1134b2404c1e3de3ab9cee95a4444c
cyanedel <cleewlkr@gmail.com>
2022-01-08 18:26:56 +0700

    fetch userlist from python
    added axios to nodejs;
    added custom route to fetch data from python port.

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var port = 3100
 const fs = require('fs');
 var path = require("path");
+const axios = require('axios');
 
 http.listen(port, function(){
 	console.log('===================');
@@ -19,18 +20,29 @@ app.get('/ping', function(req, res){
 	res.send({status: "ok", statusCode: "200"});
 });
 
+//for testing.
 app.get('/api/countrylist', function(req, res){
   let filePath = path.join(__dirname, 'countryList.json')
   let jsonList = fs.readFileSync(filePath);
   res.send(JSON.parse(jsonList))
 })
 
+//local data. to be depecrated.
 app.get('/api/userlist', function(req, res){
   let filePath = path.join(__dirname, 'userList.json')
   let jsonList = fs.readFileSync(filePath);
   res.send(JSON.parse(jsonList))
 })
 
+//get data from python api instead.
 app.get('/api/user/list', function(req, res){
-  res.send({status: "we're working on it."})
+  axios.get('http://localhost:3200/getuserlist')
+    .then( response => {
+      data = response.data
+      res.send(data)
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error)
+    })
 })
