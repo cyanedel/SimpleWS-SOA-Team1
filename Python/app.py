9dac0b8cc95ddb372a0273efb3bd7558ba1dba28
cyanedel <cleewlkr@gmail.com>
2022-01-09 03:42:54 +0700

    Add More Filter, Update UserList Data

--- Python/app.py
+++ Python/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 dirname = os.path.dirname(__file__)
 
-with open(os.path.join(dirname,'user_list.json'), errors="ignore") as f:
+with open(os.path.join(dirname,'userList.json'), errors="ignore") as f:
   userList = json.load(f)
 
 @app.get("/getuserlist")
@@ -24,8 +24,11 @@ def get_userList():
   if 'lastname' in args:
     if args['lastname'] != "":
       result = [user for user in result if args['lastname'].lower() in user.get('last_name').lower()]
+
+  if 'occupation' in args:
+    if args['occupation'] != "":
+      result = [user for user in result if args['occupation'].lower() in user.get('occupation').lower()]
   
-  print(result)
   return jsonify(result)
 
 if __name__ == '__main__':
