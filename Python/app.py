8fb9c45455eac11fe30d5fcde323ba4aa30de35d
cyanedel <cleewlkr@gmail.com>
2022-01-09 14:45:55 +0700

    Sortable Movie List
    - Sort Movie List Function
    - update userlist for python

--- Python/app.py
+++ Python/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 dirname = os.path.dirname(__file__)
 
-with open(os.path.join(dirname,'userList.json'), errors="ignore") as f:
+with open(os.path.join(dirname,'user-list.json'), errors="ignore") as f:
   userList = json.load(f)
 
 @app.get("/getuserlist")
@@ -19,11 +19,11 @@ def get_userList():
 
   if 'firstname' in args:
     if args['firstname'] != "":
-      result = [user for user in result if args['firstname'].lower() in user.get('first_name').lower()]
+      result = [user for user in result if args['firstname'].lower() in user.get('firstname').lower()]
   
   if 'lastname' in args:
     if args['lastname'] != "":
-      result = [user for user in result if args['lastname'].lower() in user.get('last_name').lower()]
+      result = [user for user in result if args['lastname'].lower() in user.get('lastname').lower()]
 
   if 'occupation' in args:
     if args['occupation'] != "":
